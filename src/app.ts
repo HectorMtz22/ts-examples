ad2b6b984a52b85763e44a9b67dad7df0d9abf49
HectorMtz22 <hec.gre81@gmail.com>
2021-06-23 00:17:20 -0500

    Basic Express Server

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+interface Individuo {
+    name: string
+    age: number
+    
+}
+
+let person: Individuo;
+console.log("Hello World!")
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+import express from 'express'
+import router from './routes'
+
+const app = express()
+const port = 3001
+
+app.use(express.json())
+
+app.use('/', router) // Imports all router
+
+app.listen(port, () => {
+    // Cuando funcione
+    console.log("Estamos al aire!")
+})
