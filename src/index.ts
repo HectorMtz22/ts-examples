b2371b7e2876da8266c96fcc5a04a69deca95270
HectorMtz22 <hec.gre81@gmail.com>
2021-06-26 22:12:48 -0500

    Basic Middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
-import express from 'express'
-import router from './routes'
+import express from "express";
+import router from "./routes";
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/', router) // Imports all routes 
+const requestTime = (
+  req: express.Request,
+  _: express.Response,
+  next: express.NextFunction
+) => {
+  const { body } = req;
+  console.log(body);
+
+  console.log(Date.now());
+  next();
+};
+
+app.use(requestTime);
+
+app.use("/", router); // Imports all routes
 
 app.listen(port, () => {
-    // Cuando funcione
-    console.log("Estamos al aire!")
-})
+  // Cuando funcione
+  console.log("Estamos al aire!");
+});
